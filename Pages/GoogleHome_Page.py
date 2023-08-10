23aa68de0069d8af97bc6032128afaf0ea5c0a70
HARSHIT <hsinha22@gmail.com>
2023-08-10 22:28:37 +0530

    Allure report implementation

--- Pages/GoogleHome_Page.py
+++ Pages/GoogleHome_Page.py
@@ -1,6 +1,7 @@
 import logging
 import time
 
+import allure
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
@@ -21,7 +22,7 @@ class GooglePage(GenericMethods):
         self.driver = driver
 
     """=====================Page Functions========================"""
-
+    @allure.title("Navigate to page")
     def navigate_to_page(self, url):
         self.navigate(url)
         if self.is_element_present(self.google_cookies_popup):
@@ -29,6 +30,7 @@ class GooglePage(GenericMethods):
         logging.info("Navigated successfully to : %s" % url)
         self.take_screenshot()
 
+    @allure.title("Type text")
     def type_text_in_search_box(self, value):
         search_box = self.find_element(self.search_textbox)
         search_box.send_keys(value)
--- Tests/test_google_page.py
+++ Tests/test_google_page.py
@@ -1,9 +1,11 @@
+import allure
 import pytest
 from Pages.GoogleHome_Page import GooglePage
 from Tests.test_base import BaseTest
 
 
 class TestGoogleSearch(BaseTest):
+    @allure.title("Search text in Google Page")
     def test_search_google(self):
         self.search_page = GooglePage(self.driver)
         self.search_page.navigate_to_page(url="https://www.google.com/")
